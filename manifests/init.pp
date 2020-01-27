00149b5ccbdd5dba87ba9096a7f5357aecb10bb3
Yorick Poels <yorick.poels@kuleuven.be>
2020-01-27 16:30:15 +0100

    Add more params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,13 @@
 # @summary Manage the `journald` daemon
 #
-# @param persist_log
-#  By default, the journal stores log data in /run/log/journal/.
+# @param Boolean persist_log # By default, the journal stores log data in /run/log/journal/.
 #  Since /run/ is volatile, log data is lost at reboot. To make the
 #  data persistent, it is sufficient to create /var/log/journal/
 #  where systemd-journald will then store the data.
-#
-# @param options
-#   A hash with journald parameters
+# @param Boolean service_enable # Enable or disable systemd journald service
+# @param Boolean service_manage # Manage the journald service or not
+# @param Hash options # A hash with journald parameters
+# @param Enum service_ensure # Set the journald service state - stopped or running
 #
 class journald (
   # Module data
