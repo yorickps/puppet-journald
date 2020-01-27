b6585085ca219da0aac56d53209374d53400741d
Yorick Poels <yorick.poels@kuleuven.be>
2020-01-27 14:47:14 +0100

    Make service manageable

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,12 @@
-class journald::service {
-  service { ['systemd-journald']:
-    ensure => running,
-    enable => true,
+# This class ensures the service is running.
+
+if $journald::service_manage == true {
+  class journald::service {
+    service { 'systemd-journald':
+      ensure     => $journald::service_ensure,
+      enable     => $journald::service_enable,
+      hasrestart => true,
+      hasstatus  => true,
+    }
   }
 }
